c37f305ebfdec1b91ea02303e33435452e3c7d96
Baakal Tesfaye <baakal@gmail.com>
2013-05-03 13:54:44 +0300

    missing code

--- EthiopianDateUserAcceptanceTest/Program.cs
+++ EthiopianDateUserAcceptanceTest/Program.cs
@@ -16,7 +16,7 @@ namespace EthiopianDateUserAcceptanceTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new EthiopianDateCode());
         }
     }
 }
